5b7d43d23c5b5905ce81aeca30bb5b47690cdcfb
Lyn0803 <longnguyen25032003@gmail.com>
2025-04-13 18:29:03 +0700

    Second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_11_150254) do
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.integer "age"
+    t.float "weight"
+    t.float "height"
+    t.string "gender"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 end
